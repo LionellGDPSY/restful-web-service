07a3b148d8780025750cad8badaea59b37b12bc6
psydev <psydev91@easymedia.net>
2023-06-06 12:56:45 +0900

    Initial commit

---
+++ src/main/java/com/example/restfulwebservice/helloworld/HelloWorldController.java
@@ -0,0 +1,41 @@
+package com.example.restfulwebservice.helloworld;
+
+import com.example.restfulwebservice.HelloWorldBean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    //Get
+    //hello-world(endpoint)
+    //RequestMapping(method=RequestMethod.GET, path="/hello-world")
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+
+        return "Hello World";
+
+    }
+
+    @GetMapping("/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+
+        //alt + Enter
+        return new HelloWorldBean("Hello World");
+
+    }
+    @GetMapping("/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldBean(@PathVariable String name) {
+
+        //alt + Enter
+        return new HelloWorldBean("Hello World, " + name);
+
+    }
+    @GetMapping("/hello-world-bean/path-variable1/{name}")
+    public HelloWorldBean helloWorldBean1(@PathVariable String name) {
+
+        //alt + Enter
+        return new HelloWorldBean(String.format("Hello World, %s" , name));
+
+    }
+}
